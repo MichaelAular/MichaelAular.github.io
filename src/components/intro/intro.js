d63ab4e4ae9cfdf8a6ea5642a4774f11ceeb9a85
MichaelAular <michaelaular@gmail.com>
2022-08-17 16:56:44 +0200

    adjustment in scaling

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -26,10 +26,11 @@ const Intro = ({ setIntro }) => {
           <b>Portfolio</b>
           <br />
           <span className="name">Michael Aular</span>
+          <div className="introBottomBlock" />
         </div>
       </div>
 
-      <div className="introBottomBlock" />
+      
     </div>
   );
 };
