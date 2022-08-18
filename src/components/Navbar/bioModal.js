33b9bb4359042c8614eb456c424406d4a4162d62
MichaelAular <michaelaular@gmail.com>
2022-08-18 10:04:22 +0200

    bio modal responsive

--- src/components/Navbar/bioModal.js
+++ src/components/Navbar/bioModal.js
@@ -1,11 +1,10 @@
-import"./aboutMe.scss";
+import "./aboutMe.scss";
 import bio from "./bio.json";
+import useWindowSize from "../../hooks/windowsize";
 
+const BioModal = ({ aboutMeOpen, setAboutMeOpen }) => {
+  const size = useWindowSize();
 
-const BioModal = ({
-  aboutMeOpen,
-  setAboutMeOpen
-  }) => {
   if (!aboutMeOpen) {
     return null;
   }
@@ -13,9 +12,16 @@ const BioModal = ({
   return (
     <div
       className="bioBlock_container font_normal"
-      onClick={() => { setAboutMeOpen(false) }}>
+      onClick={() => {
+        setAboutMeOpen(false);
+      }}
+      style={{
+       maxHeight: `calc(92vh - (${size.itemHeight}vw + 180px))`,
+      }}
+    >
       <div className="bioBlock_title font_bangers">About me</div>
       <div className="bioBlock_text font_normal">{bio.bio}</div>
+      <div className="bioPic"/>
     </div>
   );
 };
